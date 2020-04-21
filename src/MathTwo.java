4001e383425817c9ec191aeebe6394fc805fff54
tchekkol1 <tchekol1@umbc.edu>
2020-04-20 23:31:59 -0400

    file2

---
+++ src/MathTwo.java
@@ -0,0 +1,10 @@
+public class MathTwo {
+    //Write an application that assigns 2 to a variable named mynumber and 1.7938 to a variable namedmyothernumber,
+    // and then prints out the values to the console.
+    public static void main(String[] args) {
+        int mynumber=2;
+        float namedmyothernumber=1.7938f;
+        System.out.println(mynumber);
+        System.out.println(namedmyothernumber);
+    }
+}
